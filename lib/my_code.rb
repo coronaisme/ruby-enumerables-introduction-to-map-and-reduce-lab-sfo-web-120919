cc664af295df8f4f966fbb5e5fa38d9543cfb88a
Nick Corona <coronaisme@gmail.com>
2019-12-04 22:55:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,25 @@ def reduce_to_all_true(array)
   i = 0 
   
   while i < array.length do 
-    retur
+ return false if !array[i]
+ i+=1
+ end
+ return true
+ end
+ 
+ def reduce_to_any_true(array)
+   i = 0 
+   
+   while i < array.length do
+     return true if array[i]
+     i+=1 
+   end
+   return false
+ end
+     
+     
+     
+     
     
 
 
